a7a3281c75a36b1f8f43a6cfa58e3ed52c6f99a2
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2025-09-17 18:31:17 +0530

    loops

---
+++ day6/loops.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main() {
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    /*
+    1. i=1 => 1<=10 => True => Print 1
+    2. 1 => 1++ => 2<=10 => True => Print 2
+    3. 2 => 2++ => 3<=10 => True => Print 3
+    4. 3 => 3++ => 4<=10 => True => Print 4
+    5. 4 => 4++ => 5<=10 => True => Print 5
+    6. 5 => 5++ => 6<=10 => True => Print 6
+    7. 6 => 6++ => 7<=10 => True => Print 7
+    8. 7 => 7++ => 8<=10 => True => Print 8
+    9. 8 => 8++ => 9<=10 => True => Print 9
+    10. 9=> 9++ => 10<=10 => True => Print 10
+    11. 10=> 10++ =>  11<=10 => False => Loop End
+    
+    
+    */
+
+    return 0;
+}
+
+/*
+    For Loop
+    While Loop
+    Do While Loop
+
+    1. Starting Point
+    2. End Point
+    3. Increment or Decrement
+
+
+*/
---
+++ day6/test.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main() {
+
+    // int a = 10;
+    // if (a >= 10)
+    //     printf("Hello Everyone");
+
+    // else
+    //     printf("Hello World");
+
+    // a++ -  Use and then increase
+    // ++a - increase adn then use
+
+    // int a = 10;
+
+    // int b = ++a;
+
+    // printf("%d\n", b);
+    // // printf("%d", a);
+
+
+
+    int a = 10;
+    a = a + 3;
+    a += 3;
+
+    printf("%d", a);
+
+
+
+
+    return 0;
+}
+
+
+/*
+    Arthmatic Operator
+    Relational Operator
+    Logical Operator
+    Ternary Operator
+    Assigenment Operator
+
+*/
