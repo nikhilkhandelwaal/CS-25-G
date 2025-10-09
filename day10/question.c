2eebaf66ac45c94d87995ce6c640ebe20e636849
nikhilkhandelwaal <nikhilcoder061@gmail.com>
2025-10-09 17:16:24 +0530

    loops

---
+++ day10/question.c
@@ -0,0 +1,5 @@
+// Take 5 integer from scanf and print in reverse order
+// Copy elements from one array to another array
+// WAP to reverse an array with using another varible
+// WAP to reverse an array without using another varible
+// Check wheather array is palindrome or not ?
